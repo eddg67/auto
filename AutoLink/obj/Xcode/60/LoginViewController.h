5badba4cdb82eb21c6a19dda0372efb8ad36cefc
eddg67 <egalarza007@gmail.com>
2014-06-22 21:20:58 -0400

    Sycning

---
+++ AutoLink/obj/Xcode/60/LoginViewController.h
@@ -0,0 +1,30 @@
+// WARNING
+// This file has been generated automatically by Xamarin Studio to
+// mirror C# types. Changes in this file made by drag-connecting
+// from the UI designer will be synchronized back to C#, but
+// more complex manual changes may not transfer correctly.
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+@interface LoginViewController : UIViewController {
+	UIButton *_btbSignUp;
+	UIButton *_btnForgot;
+	UIButton *_btnLogin;
+	UITextField *_txtEmail;
+	UITextField *_txtPassword;
+}
+
+@property (nonatomic, retain) IBOutlet UIButton *btbSignUp;
+
+@property (nonatomic, retain) IBOutlet UIButton *btnForgot;
+
+@property (nonatomic, retain) IBOutlet UIButton *btnLogin;
+
+@property (nonatomic, retain) IBOutlet UITextField *txtEmail;
+
+@property (nonatomic, retain) IBOutlet UITextField *txtPassword;
+
+@end
